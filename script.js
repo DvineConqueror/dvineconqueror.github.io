f5b31fbf84c185ce94bea66cf2e6f5f654837949
Henry Dom Ali <114635034+DvineConqueror@users.noreply.github.com>
2024-04-14 23:09:32 +0800

    Update search bar

---
+++ script.js
@@ -0,0 +1,22 @@
+function searchFunction() {
+    var input, filter, ul, li, a, i, j;
+    input = document.getElementById("search-input");
+    filter = input.value.toUpperCase();
+    ul = document.querySelectorAll(".cards"); // Select all lists with class "cards"
+  
+    for (j = 0; j < ul.length; j++) { // Loop through each list
+      li = ul[j].getElementsByClassName("cards_item"); // Get list items for the current list
+  
+      for (i = 0; i < li.length; i++) { // Loop through each list item
+        a = li[i].getElementsByClassName("card_title")[0]; // Get the card title
+  
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+          li[i].style.display = ""; // Show the card if it matches the search criteria
+          li[i].classList.add("card-match"); // Add a class for animation
+        } else {
+          li[i].style.display = "none"; // Hide the card if it does not match the search criteria
+          li[i].classList.remove("card-match"); // Remove the animation class
+        }
+      }
+    }
+  }
